a8ec18a338eee9e99e004570d32237e7f19071e9
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-04-16 16:56:06 -0300

    netbeans file system refactoring

---
+++ src/main/java/org/openjfx/API2Semestre/Classes/AppointmentType.java
@@ -0,0 +1,11 @@
+package org.openjfx.API2Semestre.Classes;
+
+/// Define o tipo de apontamento entre Hora-Extra ou Sobreaviso
+public enum AppointmentType {
+
+    /// Hora-Extra
+    Overtime,
+
+    /// Sobreaviso
+    OnNotice,
+}
