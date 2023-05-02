63753b8b5447b9b85744fc6b5065296f74cc46f8
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-02 17:51:03 -0300

    new comments

--- src/main/java/org/openjfx/API2Semestre/database/query/QueryTable.java
+++ src/main/java/org/openjfx/API2Semestre/database/query/QueryTable.java
@@ -3,10 +3,10 @@ package org.openjfx.api2semestre.database.query;
 public enum QueryTable {
     Appointment("public.apontamento"),
     ViewAppointment("vw_apontamento");
-    // User,
-    // Client,
-    // Squad,
-    // IntervalFee,
+    // TODO: User,
+    // TODO: Client,
+    // TODO: Squad,
+    // TODO: IntervalFee,
 
     private String stringValue;
     private QueryTable (String stringValue) { this.stringValue = stringValue; }
