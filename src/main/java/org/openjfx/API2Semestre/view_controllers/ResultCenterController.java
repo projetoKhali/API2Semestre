edd2bdde3bca0a67f1e24775651b70d189f9dbe3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-06 21:48:51 -0300

    AutoCompleteTextField prototype

--- src/main/java/org/openjfx/API2Semestre/view_controllers/ResultCenterController.java
+++ src/main/java/org/openjfx/API2Semestre/view_controllers/ResultCenterController.java
@@ -1,10 +1,14 @@
 package org.openjfx.api2semestre.view_controllers;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.openjfx.api2semestre.authentication.Profile;
 import org.openjfx.api2semestre.authentication.User;
 import org.openjfx.api2semestre.custom_tags.LookupTextField;
+import org.openjfx.api2semestre.view_macros.AutocompleteTextField;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -38,26 +42,33 @@ public class ResultCenterController {
         //     User.class,
         //     new QueryParam<?>[0]
         // ));
-        colaborators = FXCollections.observableArrayList(new User[] {
+        User[] users = new User[] {
             new User("Fulano colaborador", Profile.Colaborador, List.of("oi", "oi2"), List.of()),
             new User("Fulano gestor", Profile.Gestor, List.of("oi3"), List.of("oi", "oi2")),
-            new User("Fulano adm", Profile.Administrator, List.of(), List.of("oi3"))
-        });
+            new User("Cicrano col", Profile.Colaborador, List.of(), List.of("oi3")),
+            new User("Cicrano ges", Profile.Gestor, List.of(), List.of("oi3")),
+            new User("Cicrano adm", Profile.Administrator, List.of(), List.of("oi3"))
+        };
+
+        colaborators = FXCollections.observableArrayList(users);
         // collaboratorSelect = TextFields.bindAutoCompletion(tf_colaborador, colaborators);
 
-        LookupTextField<User> lutf_colaborador = new LookupTextField<User>(colaborators);
-        ((HBox)tf_colaborador.getParent()).getChildren().set(
-            ((HBox)tf_colaborador.getParent()).getChildren().indexOf(tf_colaborador),
-            lutf_colaborador
-        );
-        tf_colaborador = lutf_colaborador;
-        // Set an event handler for when a user is selected
-        lutf_colaborador.setOnAction(event -> {
-            User user = lutf_colaborador.getSelectedItem();
-            System.out.println("User selected: " + user != null ? user.toString() : "NULL");
-        });
 
-        
+        AutocompleteTextField.setupAutocomplete(tf_colaborador, users);
+
+        // LookupTextField<User> lutf_colaborador = new LookupTextField<User>(colaborators);
+        // ((HBox)tf_colaborador.getParent()).getChildren().set(
+        //     ((HBox)tf_colaborador.getParent()).getChildren().indexOf(tf_colaborador),
+        //     lutf_colaborador
+        // );
+        // tf_colaborador = lutf_colaborador;
+        // // Set an event handler for when a user is selected
+        // lutf_colaborador.setOnAction(event -> {
+        //     User user = lutf_colaborador.getSelectedItem();
+        //     System.out.println("User selected: " + (user == null ? "NULL" : user.toString()));
+        // });
+
+
     }
 
 
---
+++ src/main/java/org/openjfx/API2Semestre/view_macros/AutocompleteTextField.java
@@ -0,0 +1,68 @@
+package org.openjfx.api2semestre.view_macros;
+
+import org.openjfx.api2semestre.authentication.User;
+
+import javafx.collections.FXCollections;
+import javafx.geometry.Bounds;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Popup;
+
+public class AutocompleteTextField {
+
+    public static void setupAutocomplete(TextField textField, User[] suggestions) {
+        ListView<User> suggestionsList = new ListView<>(FXCollections.observableArrayList(suggestions));
+        suggestionsList.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(User item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getNome());
+                }
+            }
+        });
+        Popup popup = new Popup();
+        popup.getContent().add(suggestionsList);
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.isEmpty()) {
+                popup.hide();
+            } else {
+                suggestionsList.getItems().clear();
+                for (User suggestion : suggestions) {
+                    // System.out.println(
+                    //     "suggestion(" + suggestion.getNome().toLowerCase() + 
+                    //     ") newValue(" + newValue.toLowerCase() +
+                    //     ") contains? " + suggestion.getNome().toLowerCase().contains(newValue.toLowerCase())
+                    // );
+                    if (suggestion.getNome().toLowerCase().contains(newValue.toLowerCase())) {
+                        suggestionsList.getItems().add(suggestion);
+                    }
+                }
+                if (!suggestionsList.getItems().isEmpty()) {
+                    Bounds bounds = textField.localToScreen(textField.getBoundsInLocal());
+                    double popupY = bounds.getMaxY();
+                    double cellHeight = suggestionsList.getFixedCellSize();
+                    double popupHeight = cellHeight * suggestionsList.getItems().size();
+                    suggestionsList.setMaxHeight(popupHeight);
+                    suggestionsList.setMinHeight(popupHeight);
+                    suggestionsList.setPrefHeight(popupHeight);
+                    popup.show(textField, bounds.getMinX(), popupY);
+                } else {
+                    popup.hide();
+                }
+            }
+        });
+
+        suggestionsList.setOnMouseClicked(event -> {
+            User selectedUser = suggestionsList.getSelectionModel().getSelectedItem();
+            if (selectedUser != null) {
+                textField.setText(selectedUser.getNome());
+            }
+            popup.hide();
+        });
+    }
+}
