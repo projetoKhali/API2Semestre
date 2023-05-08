a1f5ead1f48cb6ddd52a05ca6d9ff6f401dad28e
taniacruzz <tanicruz112@gmail.com>
2023-05-07 21:17:28 -0300

    calculo de tempo por verba

--- src/main/java/org/openjfx/API2Semestre/data_utils/AppointmentCalculator.java
+++ src/main/java/org/openjfx/API2Semestre/data_utils/AppointmentCalculator.java
@@ -13,70 +13,77 @@ import org.openjfx.api2semestre.appointments.AppointmentType;
 import org.openjfx.api2semestre.authentication.Authentication;
 import org.openjfx.api2semestre.database.QueryLibs;
 
+// os cálculos serão feitos em minutos, e depois divididos por 60 para se chegar a quantidade de horas (em decimal).
+
 public class AppointmentCalculator {
     
-    public static List<ReportInAppointment> calculateReports(){
-        List<ReportInAppointment> reports = new ArrayList<ReportInAppointment>();
-        public List<Appointment> appointments = new ArrayList<Appointment>();
+    static List<ReportInAppointment> reports = new ArrayList<ReportInAppointment>();
+    static List<Appointment> appointments = new ArrayList<Appointment>();
+  
+    
+    public static void calculateReports(){
+    // public static List<ReportInAppointment> calculateReports(){
         appointments = Arrays.asList(QueryLibs.collaboratorSelect(Authentication.getCurrentUser().getNome()));
-
-        
-        public List<ReportInAppointment> calculateOnNotice(Appointment apt_onNotice){
-            java.sql.Timestamp onNotice_init = apt_onNotice.getStartDate();
-            java.sql.Timestamp onNotice_end = apt_onNotice.getEndDate();
-            java.sql.Timestamp overTime_init = null;
-            java.sql.Timestamp overTime_end = null;
-
-            for(Appointment apt: appointments){
-                if (apt.getType() == AppointmentType.Overtime){
-                    overTime_init = apt.getStartDate();
-                    overTime_end = apt.getEndDate();
-                    
-                    LocalDateTime onotice_init = onNotice_init.toLocalDateTime();
-                    LocalDateTime onotice_end = onNotice_end.toLocalDateTime();
-                    LocalDateTime overtime_init = overTime_init.toLocalDateTime();
-                    LocalDateTime overtime_end = overTime_end.toLocalDateTime();
-
-                    if (onotice_end.isBefore(onotice_init) || overtime_end.isBefore(overtime_init)) {
-                        System.out.println("Not proper intervals");
-                    } else {
-                        long numberOfOverlappingDates;
-                        long numberOfOverlappingHours;
-                        long numberOfOverlappingMinutes;
-                        long numberOfOverlappingSeconds;
-                        if (onotice_end.isBefore(overtime_init) || overtime_end.isBefore(onotice_init)) {
-                            // no overlap
-                            numberOfOverlappingDates = 0;
-                            numberOfOverlappingHours = 0;
-                            numberOfOverlappingMinutes = 0;
-                            numberOfOverlappingSeconds = 0;
-                        } else {
-                            LocalDateTime laterStart = Collections.max(Arrays.asList(onotice_init, overtime_init));
-                            LocalDateTime earlierEnd = Collections.min(Arrays.asList(onotice_end, overtime_end));
-                            numberOfOverlappingDates = ChronoUnit.DAYS.between(laterStart, earlierEnd);
-                            numberOfOverlappingHours = ChronoUnit.HOURS.between(laterStart, earlierEnd);
-                            numberOfOverlappingMinutes = ChronoUnit.MINUTES.between(laterStart, earlierEnd);
-                            numberOfOverlappingSeconds = ChronoUnit.SECONDS.between(laterStart, earlierEnd);
-                        }
-                        System.out.println("" + numberOfOverlappingDates + " days of overlap");
-                        System.out.println("" + numberOfOverlappingHours + " hour of overlap");
-                        System.out.println("" + numberOfOverlappingMinutes + " minutes of overlap");
-                        System.out.println("" + numberOfOverlappingSeconds + " seconds of overlap");
-                    }
-                            
-
-
-                }
-
+        for(Appointment apt: appointments){
+            if(apt.getType() == AppointmentType.OnNotice){
+                calculateOnNotice(apt);
             }
-
-        
-
-
         }
+    }    
+    
+    public static Double calculateOnNotice(Appointment aptOnNotice){
+    // List<ReportInAppointment> calculateOnNotice(Appointment aptOnNotice){
+        java.sql.Timestamp onNotice_init = aptOnNotice.getStartDate();
+        java.sql.Timestamp onNotice_end = aptOnNotice.getEndDate();
+        LocalDateTime onotice_init = onNotice_init.toLocalDateTime();
+        LocalDateTime onotice_end = onNotice_end.toLocalDateTime();
+        java.sql.Timestamp overTime_init = null;
+        java.sql.Timestamp overTime_end = null;
+    
+        // calcular a quantidade total de tempo do sobreaviso. Obs: essa função calcula dias, horas e minutos INTEIROS.
+        long onNoticeIntervalMinutes = ChronoUnit.MINUTES.between(onotice_init, onotice_end);
+        // long onNoticeIntervalSeconds = ChronoUnit.SECONDS.between(onotice_init, onotice_end);
+    
+    
+        // achar intersecção entre sobreaviso e hora extra(acionamento)
+        for(Appointment aptOvertime: appointments){
+            if (aptOvertime.getType() == AppointmentType.Overtime){
+                overTime_init = aptOvertime.getStartDate();
+                overTime_end = aptOvertime.getEndDate();
+                LocalDateTime overtime_init = overTime_init.toLocalDateTime();
+                LocalDateTime overtime_end = overTime_end.toLocalDateTime();
+    
+                if (onotice_end.isBefore(onotice_init) || overtime_end.isBefore(overtime_init)) {
+                    System.out.println("Not proper intervals");
+                } else {
+                    long numberOfOverlappingMinutes;
+                    
+    
+                    if (onotice_end.isBefore(overtime_init) || overtime_end.isBefore(onotice_init)) {
+                        // no overlap
+                        numberOfOverlappingMinutes = 0;
+    
+                        
+                    } else {
+                        LocalDateTime laterStart = Collections.max(Arrays.asList(onotice_init, overtime_init));
+                        LocalDateTime earlierEnd = Collections.min(Arrays.asList(onotice_end, overtime_end));
+                        numberOfOverlappingMinutes = ChronoUnit.MINUTES.between(laterStart, earlierEnd);
+                        
+                    }
+                   
+                    System.out.println("" + numberOfOverlappingMinutes + " minutes of overlap");
 
-        
+                    // do total de tempo de sobreaviso, subtraio a intersecção com hora-extra
+                    onNoticeIntervalMinutes = onNoticeIntervalMinutes - numberOfOverlappingMinutes;
+                }
+            }
+        }
+        Double onNoticeIntervalMinutes_Double = (double)onNoticeIntervalMinutes;
+        Double onNoticeIntervalHoursDecimal = onNoticeIntervalMinutes_Double/60;
+        return onNoticeIntervalHoursDecimal;
     }
-    
-    
-}
+
+    public static void calculateOverTime(Appointment aptOverTime){
+
+    }
+}        
--- src/main/java/org/openjfx/API2Semestre/report/IntervalFee.java
+++ src/main/java/org/openjfx/API2Semestre/report/IntervalFee.java
@@ -1,6 +1,13 @@
 package org.openjfx.api2semestre.report;
 
+import java.io.Externalizable;
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class IntervalFee {
 
@@ -10,60 +17,99 @@ public class IntervalFee {
 
     // Condition
     private boolean[] daysOfWeek;
-    private int startHour;
-    private int endHour;
-    private int minHourCount;
+    private LocalTime startHour;
+    private LocalTime endHour;
+    private long minHourCount;
     private boolean cumulative;
-
+    
     public IntervalFee(
         int code,
-        float percent,
+        Double percent,
         boolean[] daysOfWeek,
-        int startHour,
-        int endHour,
-        int minHourCount,
+        LocalTime startHour,
+        LocalTime endHour,
+        long minHourCount,
         boolean cumulative
     ) {
         this.code = code;
         this.percent = percent;
         this.daysOfWeek = daysOfWeek;
-        this.startHour = ((startHour % 24) + 24) % 24;
-        this.endHour = ((endHour % 24) + 24) % 24;
+        this.startHour = startHour;
+        this.endHour = endHour;
         this.minHourCount = minHourCount;
         this.cumulative = cumulative;
     }
-
-    public boolean check (Timestamp appointmentStartTime, Timestamp appointmentEndTimem, double dayHourCount) {
-
+    
+    public boolean check (Timestamp appointmentStart, Timestamp appointmentEndTime, int dayHourCount) {
+        
+        // conversão de Timestamp para LocalTime
+        
+        LocalDateTime aptStartDateTime = appointmentStart.toLocalDateTime();
+        LocalTime aptStartLocalTime = aptStartDateTime.toLocalTime();
+        LocalDate aptStartLocalDate = aptStartDateTime.toLocalDate();
+        
+        LocalDateTime aptEndDateTime = appointmentEndTime.toLocalDateTime();
+        LocalTime aptEndLocalTime = aptEndDateTime.toLocalTime();
+        LocalDate aptEndLocalDate = aptEndDateTime.toLocalDate();
+        
+        long numberOfOverlappingMinutes = 0;
+        
         // Check if the day of the week falls within the configured range
-        int appointmentDayOfWeek = appointmentStartTime.toLocalDateTime().getDayOfWeek().getValue();
-        if (!daysOfWeek[appointmentDayOfWeek - 1]) { 
-            System.out.println("\tfails at: !daysOfWeek[appointmentDayOfWeek - 1]");
-            return false;
-        }
-
-        if (startHour != 0 || endHour != 0) {
-        
-            // Check if the start hour is before the configured end hour
-            int appointmentStartHour = appointmentStartTime.toLocalDateTime().getHour();
-            if (appointmentStartHour >= endHour) { 
-                System.out.println("\tfails at: appointmentStartHour(" + appointmentStartHour + ") >= endHour(" + endHour + ")");
-                return false;
+        LocalDate actualDay = aptStartLocalDate;
+        int actualDayOfWeek = actualDay.getDayOfWeek().getValue();
+        boolean auxiliar = false; 
+        while(!actualDay.isAfter(aptEndLocalDate)){
+            // uso %7 pois getDayOfWeek().getValue() considera domingo como sendo 7, e para padronizar quero que seja 0
+            if(daysOfWeek[(actualDayOfWeek % 7)]){
+                auxiliar = true;
+                break;}
+                actualDay = actualDay.plusDays(1);
+                actualDayOfWeek = actualDay.getDayOfWeek().getValue();
             }
-        
-            // Check if the end time is after the configured start hour
-            int appointmentEndHour = appointmentEndTimem.toLocalDateTime().getHour();
-            if (appointmentEndHour <= startHour) { 
-                System.out.println("\tfails at: appointmentEndHour(" + appointmentEndHour + ") <= startHour(" + startHour + ")");
-                return false;
-            }
-        }
+        if(auxiliar == false){return false;}
 
+        // casos em que a verba apenas começa a valer após as 2 horas            
         if (dayHourCount < minHourCount) { 
             System.out.println("\tfails at: dayHourCount(" + dayHourCount + ") < minHourCount(" + minHourCount + ")");
             return false;
         }
-    
+            
+        // checar se há intersecção com período noturno  
+        LocalTime meiaNoite = LocalTime.of(0, 0, 0);
+        LocalDateTime verbastart = null;
+        LocalDateTime verbaEnd = null;
+        LocalDateTime laterStart = null;
+        LocalDateTime earlierEnd = null;
+        auxiliar = false;
+        if((startHour != null) && (endHour != null)){
+            actualDay = aptStartLocalDate;
+            while(!actualDay.isAfter(aptEndLocalDate)){
+                verbastart = actualDay.atTime(startHour);
+                if(endHour.isBefore(startHour) & endHour != meiaNoite){
+                    verbaEnd = (actualDay.plusDays(1)).atTime(endHour);
+                }
+                else{
+                    verbaEnd = actualDay.atTime(endHour);
+                }
+                if(verbastart.isAfter(aptEndDateTime) || aptStartDateTime.isAfter(verbaEnd)){
+                    numberOfOverlappingMinutes = 0;
+                }
+                else{
+                    laterStart = Collections.max(Arrays.asList(verbastart, aptStartDateTime));
+                    earlierEnd = Collections.min(Arrays.asList(verbaEnd, aptEndDateTime));
+                    numberOfOverlappingMinutes += ChronoUnit.MINUTES.between(laterStart, earlierEnd);
+                    if(numberOfOverlappingMinutes != 0){auxiliar = true;};
+                }
+                actualDay = actualDay.plusDays(1);
+            }
+        }
+        if(auxiliar == false){return false;}
+        
+        if(minHourCount != 0 && numberOfOverlappingMinutes != 0){
+            numberOfOverlappingMinutes = numberOfOverlappingMinutes - minHourCount;
+            return true;
+        }
+
         // The period is within the configured range
         return true;
     }
@@ -73,9 +119,9 @@ public class IntervalFee {
     public double getPercent() { return percent; }
 
     public boolean[] getDaysOfWeek() { return daysOfWeek; }
-    public int getStartHour() { return startHour; }
-    public int getEndHour() { return endHour; }
-    public int getMinHourCount() { return minHourCount; }
+    public LocalTime getStartHour() { return startHour; }
+    public LocalTime getEndHour() { return endHour; }
+    public long getMinHourCount() { return minHourCount; }
     public boolean isCumulative() { return cumulative; }
 
     // Setters
@@ -83,8 +129,8 @@ public class IntervalFee {
     public void setPercent(double percent) { this.percent = percent; }    
     
     public void setDaysOfWeek(boolean[] daysOfWeek) { this.daysOfWeek = daysOfWeek; }
-    public void setStartHour(int startHour) { this.startHour = startHour; }
-    public void setEndHour(int endHour) { this.endHour = endHour; }
+    public void setStartHour(LocalTime startHour) { this.startHour = startHour; }
+    public void setEndHour(LocalTime endHour) { this.endHour = endHour; }
     public void setMinHourCount(int minHourCount) { this.minHourCount = minHourCount; }    
     public void setCumulative(boolean cumulative) { this.cumulative = cumulative; }
 
