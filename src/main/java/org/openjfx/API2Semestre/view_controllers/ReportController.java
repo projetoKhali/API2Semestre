1da72fb721919b7dd743db1ab06ba3eb93921d7c
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-13 09:46:37 -0300

    Fixed missing import PropertyValueFactory on ReportController.java

--- src/main/java/org/openjfx/API2Semestre/view_controllers/ReportController.java
+++ src/main/java/org/openjfx/API2Semestre/view_controllers/ReportController.java
@@ -26,6 +26,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ReportController {
 
