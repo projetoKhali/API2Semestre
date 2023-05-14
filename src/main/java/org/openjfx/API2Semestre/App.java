b4d4b2b625ca87b1a5af6a922b7d699fe035874d
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-14 20:45:02 -0300

    Integration Login | login.fxml responsive fixes

--- src/main/java/org/openjfx/API2Semestre/App.java
+++ src/main/java/org/openjfx/API2Semestre/App.java
@@ -8,16 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import org.openjfx.api2semestre.authentication.Authentication;
-import org.openjfx.api2semestre.authentication.Profile;
 import org.openjfx.api2semestre.view_controllers.BaseController;
 import org.openjfx.api2semestre.view_controllers.templates.ViewButtonController;
 import org.openjfx.api2semestre.views_manager.View;
 import org.openjfx.api2semestre.views_manager.ViewsManager;
 
 public class App extends Application {
-    
-    // mude o perfil de acesso para logar com diferentes permissões
-    private static final Profile access = Profile.Colaborador;
     
     private static Scene scene;
     private static Stage stage;
@@ -30,56 +26,17 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        // org.openjfx.api2semestre.database.QueryLibs.executeSqlFile("./SQL/tabelas.sql");
-        // org.openjfx.api2semestre.database.QueryLibs.executeSqlFile("./SQL/views.sql");
-
-        // org.openjfx.api2semestre.database.QueryLibs.insertUser(
-        //     new org.openjfx.api2semestre.authentication.User(
-        //         "Fulano colaborador 0", Profile.Colaborador, "e@xem.plo", "0", "0"
-        //     )
-        // );
-        // org.openjfx.api2semestre.database.QueryLibs.insertUser(
-        //     new org.openjfx.api2semestre.authentication.User(
-        //         "Fulano gestor 0", Profile.Gestor, "e@xem.plo", "0", "0"
-        //     )
-        // );
-        // org.openjfx.api2semestre.database.QueryLibs.insertUser(
-        //     new org.openjfx.api2semestre.authentication.User(
-        //         "Cicrano col 0", Profile.Colaborador, "e@xem.plo", "0", "0"
-        //     )
-        // );
-        // org.openjfx.api2semestre.database.QueryLibs.insertUser(
-        //     new org.openjfx.api2semestre.authentication.User(
-        //         "Cicrano ges 0", Profile.Gestor, "e@xem.plo", "0", "0"
-        //     )
-        // );
-
         setStage(stage);
- 
-        // try {
-        //     stage.setScene(new Scene(loadFXML("views/resultCenterRegister")));
-        //     stage.show();
-        // } catch (IOException e) {
-        //     // Auto-generated catch block
-        //     e.printStackTrace();
-        // }
-        
-        // loginView();
+         
+        loginView();
     }
 
     public static void loginView () {
-
-        currentViewFxmlFile = (
-            access == Profile.Administrator ? "login/provisory_adm" : 
-            access == Profile.Gestor ? "login/provisory_ges" : 
-            "login/provisory_col"
-        );
-        
         try {
-            scene = new Scene(loadFXML(currentViewFxmlFile));
-
+            scene = new Scene(loadFXML((currentViewFxmlFile = "views/login")));
             stage.setScene(scene);
             stage.show();
+            System.out.println(currentViewFxmlFile);
 
         } catch (Exception ex) {
             System.out.println("App.loginView() -- Erro!");
@@ -136,55 +93,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-
-        // // verbas teste
-        // IntervalFee[] verbas = new IntervalFee[] {
-
-        //     // sem restrição de período / verba base
-        //     new IntervalFee(1000, 1.00f, Week.ALL.get(), 0, 0, 0, false),
-
-        //     // final de semana (sabado e domingo)
-        //     new IntervalFee(1001, 1.25f, Week.FDS.get(), 0, 0, 0, false),
-
-        //     // qualquer dia, noturno | cumulativo
-        //     new IntervalFee(1002, 1.47f, Week.ALL.get(), 22, 6, 0, true),
-
-        //     // qualquer dia, após 2 horas de hora-extra | cumulativo
-        //     new IntervalFee(1002, 2.00f, Week.ALL.get(), 0, 0, 2, true)
-        // };
-
-        // Timestamp[][] testTimestamps = new Timestamp[][] {
-        //     new Timestamp[] {
-        //         new Timestamp(2023, 4, 30, 11, 0, 0, 0),
-        //         new Timestamp(2023, 4, 30, 12, 0, 0, 0)
-        //     },
-        //     new Timestamp[] {
-        //         new Timestamp(2023, 5, 1, 11, 0, 0, 0),
-        //         new Timestamp(2023, 5, 1, 12, 0, 0, 0)
-        //     },
-        //     new Timestamp[] {
-        //         new Timestamp(2023, 5, 1, 23, 30, 0, 0),
-        //         new Timestamp(2023, 5, 2, 0, 30, 0, 0)
-        //     }
-        // };
-
-        // exemplo
-        // double sum = 0;
-        // for (int i = 0; i < testTimestamps.length; i++) {
-        //     Timestamp[] start_end = testTimestamps[i];
-        //     for (IntervalFee verba : verbas) {
-        //         System.out.println("["+i+"] verba " + verba.getCode() + " | verificando :)");
-
-        //         if (verba.check(start_end[0], start_end[1], sum) ) {
-        //             System.out.println("["+i+"] verba " + verba.getCode() + " aplica-se a " + start_end[0].toString() + " e " + start_end[1].toString());
-        //         }
-        //         else System.out.println("["+i+"] verba " + verba.getCode() + " NÃO se aplica a " + start_end[0].toString() + " e " + start_end[1].toString());
-        //         // sum += total;
-        //     }
-        // }
-        // System.exit(1);
-
-        // System.setProperty("javafx.fxml.debug", "true");
+        System.setProperty("javafx.fxml.debug", "true");
         launch();
     }
 
---
+++ src/main/java/org/openjfx/API2Semestre/view_controllers/LoginController.java
@@ -0,0 +1,27 @@
+package org.openjfx.api2semestre.view_controllers;
+
+import org.openjfx.api2semestre.App;
+import org.openjfx.api2semestre.authentication.Authentication;
+import org.openjfx.api2semestre.authentication.ProvisoryLogin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML private TextField tf_email;
+    @FXML private TextField tf_senha;
+   
+    public void login() {
+        try {
+            String email = tf_email.getText();
+            String password = tf_senha.getText();
+            if (Authentication.login(email, password)) {
+                App.changeView(ProvisoryLogin.handleViews());
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao efetuar login");
+            e.printStackTrace();
+        }
+    }
+}
