9cd4cb3e2ad2c5ae61f8a0c2d2c4209d25bee46f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-14 21:10:05 -0300

    Refactoring

--- src/main/java/org/openjfx/API2Semestre/view_controllers/ClientsController.java
+++ src/main/java/org/openjfx/API2Semestre/view_controllers/ClientsController.java
@@ -26,7 +26,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-public class ClienteRegisterController implements Initializable {
+public class ClientsController implements Initializable {
     
     @FXML private TextField cx_razao;
     @FXML private TextField cx_cnpj;
