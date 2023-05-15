d8d68b79ffaeca27c77f87a4bc2d6f1815f3f448
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-14 21:19:44 -0300

    Refactoring QueryTable

--- src/main/java/org/openjfx/API2Semestre/database/query/QueryTable.java
+++ src/main/java/org/openjfx/API2Semestre/database/query/QueryTable.java
@@ -1,19 +1,20 @@
 package org.openjfx.api2semestre.database.query;
 
-import org.openjfx.api2semestre.authentication.User;
-
 public enum QueryTable {
+
     Appointment("public.apontamento"),
     ViewAppointment("vw_apontamento"),
+
     User("public.usuario"),
     // ViewUser(""),
+
     ResultsCenter("public.centro_resultado"),
     // ViewResultsCenter("");
-    Client("public.cliente");
-    // TODO: User,
-    // TODO: Client,
-    // TODO: Squad,
+
+    Client("public.cliente"),
+
     // TODO: IntervalFee,
+    ;
 
     private String stringValue;
     private QueryTable (String stringValue) { this.stringValue = stringValue; }
