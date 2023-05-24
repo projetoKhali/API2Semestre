1d22bb4d308653f2b401a2f4a3510fb9a2633c5b
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-05-24 03:08:11 -0300

    Added Dashboard to view.manager.View.java | Added comments to view.manager.ViewsManager.java

--- src/main/java/org/openjfx/API2Semestre/view/manager/ViewsManager.java
+++ src/main/java/org/openjfx/API2Semestre/view/manager/ViewsManager.java
@@ -12,41 +12,62 @@ import org.openjfx.api2semestre.authentication.User;
 import org.openjfx.api2semestre.database.QueryLibs;
 
 public class ViewsManager {
+
+    /// Array contendo todas as telas que o usuário logado tem acesso
     private static View[] views;
 
+    /// Retorna as telas do usuário que foram previamente definidas durante o login
     public static View[] getViews() {
         return views;
     }
 
-    public static void setViews(View[] views) {
-        ViewsManager.views = views;
-    }
-
+    /// Cria a array de telas que o usuário logado possui acesso 
     public static Optional<String> handleViews () throws Exception {
+
+        // puxa o usuário logado no momento
         final User user = Authentication.getCurrentUser();
 
+        // puxa as permissões do usuário
         Permission[] userPermissions = getPermissions(user);
 
+        // inicializa uma lista de View
         List<View> userViews = new ArrayList<>();
+
+        // inicializa uma View como primeira view para ser aberta como padrão ao iniciar o programa
         View firstView = null;
+
+        // itera através de todas as views definidas em View.java
         for (View view : View.VIEWS) {
+
+            // cria uma lista temporária dentro do if statement contendo todas as views
+            // salvas atualmente em userViews filtrando apenas as que possuam uniqueID
+            // igual ao uniqueID da view atual do loop. Caso essa lista não esteja vazia,
+            // pula a view atual do loop.
+            // Isso impede que sejam adicionadas mais de uma view com o mesmo uniqueID em userViews.
             if (userViews.stream().filter(
                 repeatedView -> repeatedView.getUniqueID() == view.getUniqueID()
-            )
-            .collect(Collectors.toList()).size() > 0) continue;
+            ).collect(Collectors.toList()).size() > 0) continue;
+
+            // pula caso o usuário não possua acesso a view atual do loop de acordo com suas permissões
             if (!view.userHasAccess(userPermissions)) continue;
+
+            // uniqueID não repete e usuário possui acesso a view. Adiciona essa view à userViews
             userViews.add(view);
-            if (firstView == null) firstView = view;
+
         }
 
-        if (userViews.isEmpty() || firstView == null) {
-            System.out.println("ProvisoryLogin.handleViews() -- Error: User has no permissions!");
-            ViewsManager.setViews(new View[0]);
+        // Erro e Optional.empty() como retorno caso userViews esteja vazia
+        if (userViews.isEmpty()) {
+            System.out.println("ViewsManager.handleViews() -- Error: User has no permissions!");
+            views = new View[0];
             return Optional.empty();
         }
 
-        ViewsManager.setViews(userViews.toArray(View[]::new));
-        return Optional.of(firstView.getFxmlFileName());
+        // userViews não está vazia, define a primeira view de userViews como firstView
+        firstView = userViews.get(0);
+
+        views = userViews.toArray(View[]::new);
+        return firstView == null ? Optional.empty() : Optional.of(firstView.getFxmlFileName());
 
     }
 
