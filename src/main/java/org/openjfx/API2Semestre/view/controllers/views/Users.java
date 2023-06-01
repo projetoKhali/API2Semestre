cbc0fb979ac9f8c5c61993bb08058685638da100
Matheus Almeida <matheusalmsou@gmail.com>
2023-06-01 20:37:25 -0300

    Adicionado botão de deletar nas colunas das tabelas das telas Usuário e Cliente, e célular modificadas para editáveis

--- src/main/java/org/openjfx/API2Semestre/view/controllers/views/Users.java
+++ src/main/java/org/openjfx/API2Semestre/view/controllers/views/Users.java
@@ -3,7 +3,12 @@ package org.openjfx.api2semestre.view.controllers.views;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableSelectionModel;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +16,7 @@ import java.util.Optional;
 import org.openjfx.api2semestre.authentication.Profile;
 import org.openjfx.api2semestre.authentication.User;
 import org.openjfx.api2semestre.database.QueryLibs;
+import org.openjfx.api2semestre.database.SQLConnection;
 import org.openjfx.api2semestre.view.macros.ColumnConfig;
 import org.openjfx.api2semestre.view.macros.ColumnConfigString;
 import org.openjfx.api2semestre.view.macros.TableMacros;
@@ -23,10 +29,16 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import org.openjfx.api2semestre.view.macros.TableMacros.Formatter;
 
@@ -36,18 +48,15 @@ public class Users {
     @FXML private TextField tf_name;
     @FXML private TextField tf_email;
     @FXML private TextField tf_matricula;   
-    @FXML private Button btCadastrar;
-    @FXML private Button btEditar;
-    @FXML private Button btRemover;
-
 
     @FXML private TableView<User> tabela;
 
     @FXML private TableColumn<User, String> col_matricula;
     @FXML private TableColumn<User, String> col_name;
     @FXML private TableColumn<User, String> col_email;
-    @FXML private TableColumn<User, String> col_function;
-    
+    @FXML private TableColumn<User, Profile> col_function;
+    @FXML private AnchorPane confirmation;
+
     private BooleanProperty col_matricula_enableFilter = new SimpleBooleanProperty();
     private BooleanProperty col_name_enableFilter = new SimpleBooleanProperty();
     private BooleanProperty col_email_enableFilter = new SimpleBooleanProperty();
@@ -66,10 +75,46 @@ public class Users {
             (String value) -> ! value.isBlank(),
             (User item, String value) -> item.setMatricula(value),
             new Formatter<String>() {
-                private final StringConverter<String> converter = new StringConverter<String>();
-                @Override public String format(String value, boolean editing) { return converter.toString(value); }
+                private final StringConverter<String> converter = null;
+                @Override public String format(String value, boolean editing) { return value; }
                 @Override public String parse(String text) { return text; }
                 @Override public StringConverter<String> getConverter() { return converter; }
+            }
+        );
+
+        TableMacros.<User, String>enableEditableCells(
+            col_name,
+            (String value) -> ! value.isBlank(),
+            (User item, String value) -> item.setNome(value),
+            new Formatter<String>() {
+                private final StringConverter<String> converter = null;
+                @Override public String format(String value, boolean editing) { return value; }
+                @Override public String parse(String text) { return text; }
+                @Override public StringConverter<String> getConverter() { return converter; }
+            }
+        );
+
+        TableMacros.<User, String>enableEditableCells(
+            col_email,
+            (String value) -> ! value.isBlank(),
+            (User item, String value) -> item.setEmail(value),
+            new Formatter<String>() {
+                private final StringConverter<String> converter = null;
+                @Override public String format(String value, boolean editing) { return value; }
+                @Override public String parse(String text) { return text; }
+                @Override public StringConverter<String> getConverter() { return converter; }
+            }
+        );
+
+        TableMacros.<User, Profile>enableEditableCells(
+            col_function,
+            (Profile value) -> true,
+            (User item, Profile value) -> item.setPerfil(value),
+            new Formatter<Profile>() {
+                private final StringConverter<Profile> converter = null;
+                @Override public String format(Profile value, boolean editing) { return value.getDisplayName(); }
+                @Override public String parse(String text) { return text; }
+                @Override public StringConverter<Profile> getConverter() { return converter; }
             }
         );
 
@@ -77,6 +122,25 @@ public class Users {
 
     @SuppressWarnings("unchecked")
     private void buildTable () {
+
+        TableColumn<User, Void> buttonColumn = new TableColumn<>("Opção");
+        tabela.getColumns().add(buttonColumn);
+
+        buttonColumn.setCellFactory(param -> new TableCell<>() {
+            
+            private final Button button = new Button("Deletar");
+
+            {
+                button.setOnAction(event -> {
+                    try {
+                        deleteUser();
+                    } catch (IOException | SQLException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
 
         cb_profile.setItems(FXCollections.observableArrayList(Profile.PROFILES));
         cb_profile.setValue(Profile.Colaborador);
@@ -153,10 +217,26 @@ public class Users {
         updateTable();
     }
     
-    public void deleteUser() {
+    private void deleteUser() throws IOException, SQLException {
         
-        User user = tabela.getSelectionModel().getSelectedItem();
+        Stage stage = (Stage) confirmation.getScene().getWindow();
         
+        Alert.AlertType type = Alert.AlertType.CONFIRMATION;
+        Alert alert = new Alert(type, "");
+
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.initOwner(stage);
+
+        alert.getDialogPane().setContentText("Tem certeza que deseja excluir este usuário?");
+        alert.getDialogPane().setHeaderText("Excluir usuário: ");
+
+        Optional<ButtonType> result = alert.showAndWait();
         
+        if(result.get() == ButtonType.OK) {
+        
+            User selectedUser = tabela.getSelectionModel().getSelectedItem();
+
+            QueryLibs.deleteUser(selectedUser); 
+        }
     }   
 }
