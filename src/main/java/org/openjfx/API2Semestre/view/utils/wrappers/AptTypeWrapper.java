648cf36490947fe5c4bfadda6bfadbb6a9625d51
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-06-03 20:45:45 -0300

    Added AptTypeWrapper.java

---
+++ src/main/java/org/openjfx/API2Semestre/view/utils/wrappers/AptTypeWrapper.java
@@ -0,0 +1,27 @@
+package org.openjfx.api2semestre.view.utils.wrappers;
+
+import java.util.Optional;
+
+import org.openjfx.api2semestre.appointment.AppointmentType;
+import org.openjfx.api2semestre.data.HasDisplayName;
+
+public class AptTypeWrapper implements HasDisplayName {
+    private Optional<AppointmentType> typeOptional;
+
+    public AptTypeWrapper(Optional<AppointmentType> typeOptional) {
+        this.typeOptional = typeOptional;
+    }
+
+    @Override public String getName() {
+        return typeOptional.isPresent() ? typeOptional.get().getStringValue() : "Todos";
+    }
+
+    public static final AptTypeWrapper[] all () {
+        return new AptTypeWrapper[] {
+            new AptTypeWrapper(Optional.empty()),
+            new AptTypeWrapper(Optional.of(AppointmentType.Overtime)),
+            new AptTypeWrapper(Optional.of(AppointmentType.OnNotice))
+        };
+    }
+    
+}
