2e6767546f6490eacc496b625c5cf2f9b90fd671
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-06-04 00:12:10 -0300

    Added TableMacros.createDeleteColumn

--- src/main/java/org/openjfx/API2Semestre/view/macros/TableMacros.java
+++ src/main/java/org/openjfx/API2Semestre/view/macros/TableMacros.java
@@ -2,12 +2,19 @@ package org.openjfx.api2semestre.view.macros;
 
 import java.util.Optional;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+
 import javafx.beans.value.ChangeListener;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.KeyCode;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
 public class TableMacros {
@@ -22,8 +29,49 @@ public class TableMacros {
         }
     }
 
+    @FunctionalInterface public static interface DeleteConfirmationCallback<T> {
+        void execute(T item);
+    }
+
+    public static <T> void createDeleteColumn(TableView<T> table, DeleteConfirmationCallback<T> deleteConfirmationCallback) {
+
+        TableColumn<T, Void> buttonColumn = new TableColumn<>("Opção");
+        buttonColumn.setMaxWidth(64);
+        buttonColumn.setResizable(false);
+        buttonColumn.setEditable(false);
+        buttonColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button button = new Button("Deletar"); {
+                button.setOnAction(event -> {
+                    try {
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "");
+    
+                        alert.initModality(Modality.APPLICATION_MODAL);
+                        alert.initOwner((Stage) table.getScene().getWindow());
+                
+                        alert.getDialogPane().setContentText("Tem certeza que deseja excluir este usuário?");
+                        alert.getDialogPane().setHeaderText("Excluir usuário: ");
+                
+                        Optional<ButtonType> result = alert.showAndWait();
+                
+                        if(result.get() != ButtonType.OK) return;
+
+                        deleteConfirmationCallback.execute(table.getItems().get(getIndex()));
+                    }
+                    catch (Exception e) { e.printStackTrace(); }
+                });
+            }
+            @Override protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) setGraphic(null);
+                else setGraphic(button);
+            }
+        });
+        table.getColumns().add(buttonColumn);
+    }
+
     public static <S, T> void enableEditableCells(TableColumn<S, T> column, Validator<T> validator, Updater<S, T> updater, Formatter<T> formatter) {
 
+        column.getTableView().setEditable(true);
         column.setEditable(true);
 
         column.setCellFactory(c -> {
