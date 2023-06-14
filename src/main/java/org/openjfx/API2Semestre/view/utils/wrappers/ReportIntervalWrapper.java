c7506b539a95b80255fcfcbaa6cd65b5fb838ee0
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-06-14 09:25:44 -0300

    Added Date formatting to ReportIntervalWrapper

--- src/main/java/org/openjfx/API2Semestre/view/utils/wrappers/ReportIntervalWrapper.java
+++ src/main/java/org/openjfx/API2Semestre/view/utils/wrappers/ReportIntervalWrapper.java
@@ -16,14 +16,18 @@ public class ReportIntervalWrapper {
     public Appointment getAppointment() { return appointment; }
     public ReportInterval getInterval() { return interval; }
 
-    public String getRequesterRegistration() { return appointment.getRequesterRegistration() ; }
-    public String getRequesterName() { return appointment.getRequesterName() ; }
-    public String getIntervalFeeCode() { return Integer.toString(interval.getVerba()) ; }
-    public String getStart() { return interval.getStart().toString() ; }
-    public String getEnd() { return interval.getEnd().toString() ; }
+    public String getRequesterRegistration() { return appointment.getRequesterRegistration(); }
+    public String getRequesterName() { return appointment.getRequesterName(); }
+    public String getIntervalFeeCode() { return Integer.toString(interval.getVerba()); }
+    public String getStart() {
+        return new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm").format(new java.util.Date(interval.getStart().getTime()));
+    }
+    public String getEnd() {
+        return new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm").format(new java.util.Date(interval.getEnd().getTime()));
+    }
     public String getTotal() { return interval.getTotalString(); }
-    public String getResultCenterName() { return appointment.getResultCenterName() ; }
-    public String getClientName() { return appointment.getClientName() ; }
-    public String getProjectName() { return appointment.getProject() ; }
-    public String getJustification() { return appointment.getJustification() ; }
+    public String getResultCenterName() { return appointment.getResultCenterName(); }
+    public String getClientName() { return appointment.getClientName(); }
+    public String getProjectName() { return appointment.getProject(); }
+    public String getJustification() { return appointment.getJustification(); }
 }
